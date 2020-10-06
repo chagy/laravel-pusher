89348aa5ec6e3c192bfd6afce062ca3c17b3e7c3
chagy <chagy_x_@hotmail.com>
2020-10-06 19:02:11 +0700

    5-Crud for Question

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -8,6 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Question extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'body',
+        'category_id',
+        'user_id'
+    ];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
     
     public function user()
     {
@@ -22,5 +35,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
