3b45a3a2be955610d186b3415d4a9a15f4d42286
chagy <chagy_x_@hotmail.com>
2020-10-06 19:50:56 +0700

    6-CRUD for Categories

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CategoryController;
 
 $namespacController = "\App\Http\Controllers\\";
 
 Route::apiResource('/question',$namespacController.'QuestionController');
+Route::apiResource('/category',CategoryController::class);
