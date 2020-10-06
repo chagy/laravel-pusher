136cc839813cc295d72f84ce2f022744b1ccf568
chagy <chagy_x_@hotmail.com>
2020-10-06 21:03:42 +0700

    7-CRUD for Reply

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,6 +6,7 @@ use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use App\Http\Resources\CategoryResource;
 
 class CategoryController extends Controller
 {
@@ -53,7 +54,7 @@ class CategoryController extends Controller
      */
     public function show(Category $category)
     {
-        return $category;
+        return new CategoryResource($category);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -2,9 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ReplyController;
 use App\Http\Controllers\CategoryController;
 
 $namespacController = "\App\Http\Controllers\\";
 
 Route::apiResource('/question',$namespacController.'QuestionController');
 Route::apiResource('/category',CategoryController::class);
+Route::apiResource('/question/{question}/reply',ReplyController::class);
